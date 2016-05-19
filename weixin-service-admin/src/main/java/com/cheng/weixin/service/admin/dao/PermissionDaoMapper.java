4c9047ef5f5c57ea700e6eb8851eee664b459574
cheng <chengzhx76@qq.com>
2016-05-19 17:57:49 +0800

    modify

---
+++ weixin-service-admin/src/main/java/com/cheng/weixin/service/admin/dao/PermissionDaoMapper.java
@@ -0,0 +1,15 @@
+package com.cheng.weixin.service.admin.dao;
+
+import com.cheng.weixin.common.core.dao.BaseDaoMapper;
+import com.cheng.weixin.rpc.admin.entity.Admin;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Desc: 管理员mapper
+ * Author: Cheng
+ * Date: 2016/1/28 0028
+ */
+@Repository
+public interface PermissionDaoMapper extends BaseDaoMapper<Admin> {
+
+}
