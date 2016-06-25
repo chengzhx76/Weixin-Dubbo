e6b5f3ae302ab3e44f9577678182153905aa05f2
chengzhx76 <chengzhx76@qq.com>
2016-06-25 11:03:28 +0800

    mobile

--- weixin-web-mobile/src/main/webapp/html/js/deliver.js
+++ weixin-web-mobile/src/main/webapp/html/js/deliver.js
@@ -1,3 +1,17 @@
 /**
  * Created by 光灿 on 2016/6/20.
+ * http://www.freejs.net/article_tabbiaoqian_29.html
  */
+$(function () {
+    $('section').hide();
+    $(".weui_navbar .weui_navbar_item:first").addClass("tab-red").show();
+    $("section:first").show();
+    $(".weui_navbar_item").click(function() {
+        $(".weui_navbar_item").removeClass("tab-red");
+        $(this).addClass("tab-red");
+        $("section").hide();
+        var activeTab = $(this).find("a").attr("href");
+        $(activeTab).fadeIn();
+        return false;
+    });
+});
