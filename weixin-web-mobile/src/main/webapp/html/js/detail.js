170cd2753a98d4e1ff9f66cfc7926937e70a5270
chengzhx76 <chengzhx76@qq.com>
2016-06-27 22:24:29 +0800

    bug

--- weixin-web-mobile/src/main/webapp/html/js/detail.js
+++ weixin-web-mobile/src/main/webapp/html/js/detail.js
@@ -15,4 +15,84 @@ $(function () {
             me.find('.dot').children().eq(i).addClass('cur').siblings().removeClass('cur');
         }
     });
+
+    // 增加事件
+    $(".add").click(function () {
+        var obj = $(this);
+        var num = parseInt(obj.prev('.count').text());
+        if (num >= 999) { // 最多支持购买999个
+            $.alert("商品不能超过999个","提示");
+            return;
+        }
+        addCount(obj);
+        addTotalCount(obj);
+
+        addTotalPrice(obj);
+    });
+// 减少事件
+    $(".sub").click(function () {
+        var obj = $(this);
+
+        var numObj = obj.siblings('.count');
+        var num = parseInt(numObj.text());
+        if (num<=1) {
+            $.alert("至少得购买一个","提示");
+            return;
+        }
+
+        subCount(obj);
+        subTotalCount(obj);
+
+        subTotalPrice(obj);
+    })
+    // 添加数量
+    function addCount(obj) {
+        var numObj = obj.siblings('.count');
+        var num = parseInt(numObj.text());
+        num += 1;
+        numObj.text(num);
+    };
+    // 增加总个数
+    function addTotalCount(obj) {
+        var totalNum = parseInt($('#count').text().trim());
+        var num  = totalNum % 9;
+        if(!num) {
+            totalNum += 2
+        } else {
+            totalNum += 1;
+        }
+        $('#count').text(totalNum);
+    }
+    // 添加商品金额
+    function addTotalPrice(obj) {
+        var totalPrice = parseFloat($('#amount').text().trim());
+        var singlePrice = parseFloat($('#single').text().trim());
+        totalPrice += singlePrice;
+        $('#amount').text(totalPrice.toFixed(1));
+    }
+    // 减少数量
+    function subCount(obj) {
+        var numObj = obj.siblings('.count');
+        var num = parseInt(numObj.text());
+        num -= 1;
+        numObj.text(num);
+    };
+    // 减少总个数
+    function subTotalCount(obj) {
+        var totalNum = parseInt($('#count').text().trim());
+        var num  = totalNum % 9;
+        if(!num) {
+            totalNum -= 2
+        } else {
+            totalNum -= 1;
+        }
+        $('#count').text(totalNum);
+    }
+    // 添加商品金额
+    function subTotalPrice(obj) {
+        var totalPrice = parseFloat($('#amount').text().trim());
+        var singlePrice = parseFloat($('#single').text().trim());
+        totalPrice -= singlePrice;
+        $('#amount').text(totalPrice.toFixed(1));
+    }
 });
