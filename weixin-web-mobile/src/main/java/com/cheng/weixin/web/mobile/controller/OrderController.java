90c7e05b20c2d76296a5a575c584c865effa25f3
chengzhx76 <chengzhx76@qq.com>
2016-09-21 17:34:07 +0800

    订单页面

--- weixin-web-mobile/src/main/java/com/cheng/weixin/web/mobile/controller/OrderController.java
+++ weixin-web-mobile/src/main/java/com/cheng/weixin/web/mobile/controller/OrderController.java
@@ -1,7 +1,10 @@
 package com.cheng.weixin.web.mobile.controller;
 
 import com.cheng.weixin.web.mobile.model.Response;
+import com.cheng.weixin.web.mobile.result.order.SubmitOrderInfo;
 import com.cheng.weixin.web.mobile.security.IgnoreSecurity;
+import com.cheng.weixin.web.mobile.service.SysOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("order")
 public class OrderController extends BaseController {
 
+    @Autowired
+    private SysOrderService orderService;
+
     /** 购买 **/
     @IgnoreSecurity
     @RequestMapping(value = "v1/buy")
     public ResponseEntity<Response> buy() {
-
-        return success();
+        SubmitOrderInfo orderInfo = orderService.buy();
+        return success(orderInfo);
     }
 }
