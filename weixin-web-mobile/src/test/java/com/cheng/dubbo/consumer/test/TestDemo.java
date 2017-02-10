b1773df6e03e39dddf6909201106ec2bb62fb128
chengzhx76 <chengzhx76@qq.com>
2017-02-10 23:24:48 +0800

    d

---
+++ weixin-web-mobile/src/test/java/com/cheng/dubbo/consumer/test/TestDemo.java
@@ -0,0 +1,24 @@
+package com.cheng.dubbo.consumer.test;
+
+import java.util.Date;
+
+/**
+ * Desc:
+ * Author: 光灿
+ * Date: 2017/1/29
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+
+
+        Date date = new Date(1485619200000L);
+        System.out.println(date);
+    }
+
+
+
+}
+
+
