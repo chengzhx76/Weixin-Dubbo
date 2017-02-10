0d384e42d4bacc58262cfd18081c2e4fa685bace
chengzhx76 <chengzhx76@qq.com>
2017-02-10 23:27:17 +0800

    d

--- weixin-web-mobile/src/test/java/com/cheng/dubbo/consumer/test/TestDemo.java
+++ weixin-web-mobile/src/test/java/com/cheng/dubbo/consumer/test/TestDemo.java
@@ -11,8 +11,6 @@ public class TestDemo {
 
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
-
-
         Date date = new Date(1485619200000L);
         System.out.println(date);
     }
